00b6b82442183348b1a5a54dd72ea71a12c39756
Jimmy Zhu <zhenyzhu@Microsoft.com>
2021-06-14 22:05:55 -0700

    Learning JS.

--- wwwroot/js/two-sum.js
+++ wwwroot/js/two-sum.js
@@ -3,20 +3,34 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    var result = [];
+const twoSumBF = function(nums, target) {
+    let result = [];
 
-    for (var i = 0; i < nums.length; i++) {
-        for (var j = i + 1; j < nums.length; j++) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
                 result.push(i);
                 result.push(j);
+
+                return result;
             }
         }
     }
-
-    return result;
 };
 
-console.log(twoSum([2, 7, 11, 15], 9));
-document.getElementById('two-sum').innerHTML = twoSum([2, 7, 11, 15], 9);
+const twoSumMap = function(nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let val = target - nums[i];
+        if (map.has(val)) {
+            let j = map.get(val);
+
+            return [j, i];
+        }
+
+        map.set(nums[i], i);
+    }
+}
+
+console.log(twoSumMap([2, 7, 11, 15], 9));
+document.getElementById('two-sum').innerHTML = twoSumMap([2, 7, 11, 15], 13);
