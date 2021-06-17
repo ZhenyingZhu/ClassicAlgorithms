03dfd4739d65e8d4fdece95c6067975787244f7f
Jimmy Zhu <zhenyzhu@Microsoft.com>
2021-06-16 22:22:47 -0700

    Fix 2-sum bug.

--- wwwroot/js/two-sum.js
+++ wwwroot/js/two-sum.js
@@ -30,22 +30,28 @@ const twoSumMap = function(nums, target) {
 
         map.set(nums[i], i);
     }
-}
+};
 
 const twoSumSort = function(nums, target) {
-    nums.sort(function(a, b) {
-        return a - b
+    indexes = [];
+    for (let i = 0; i < nums.length; i++) {
+        const num_idx = {num: nums[i], idx: i};
+        indexes.push(num_idx);
+    }
+
+    indexes.sort(function(a, b) {
+        return a.num - b.num
     });
 
     let i = 0;
     let j = nums.length - 1;
 
     while (i < j) {
-        if (nums[i] + nums[j] === target) {
-            return [i, j];
+        if (indexes[i].num + indexes[j].num === target) {
+            return [indexes[i].idx, indexes[j].idx];
         }
 
-        if (nums[i] + nums[j] < target) {
+        if (indexes[i].num + indexes[j].num < target) {
             i++;
         }
         else
@@ -53,7 +59,16 @@ const twoSumSort = function(nums, target) {
             j--;
         }
     }
-}
+};
+
+/* Test cases
+[2,7,11,15]
+9
+[3,2,4]
+6
+[3,3]
+6
+*/
 
 // console.log(twoSumSort([2, 7, 11, 15], 9));
 document.getElementById('two-sum').innerHTML = twoSumSort([2, 7, 11, 15], 9);
