0f687ca5f7cc19644ae434163730cfe0acfdc373
Zhenying Zhu <zz2283@columbia.edu>
2021-06-17 23:58:19 -0700

    Trying to deal with a list node.

--- wwwroot/js/add-two-numbers.js
+++ wwwroot/js/add-two-numbers.js
@@ -9,6 +9,69 @@ function ListNode(val, next) {
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-    let head = new ListNode(undefined, undefined);
+const addTwoNumbers = function(l1, l2) {
+    if (l1 === null) {
+        return l2;
+    }
+    if (l2 === null) {
+        return l1;
+    }
+
+    let dummy = new ListNode(0, null);
+    let prev = dummy;
+
+    let val = 0;
+    while (l1 !== null && l2 !== null) {
+        val = val + l1.val + l2.val;
+        l1 = l1.next;
+        l2 = l2.next;
+
+        let curr = new ListNode(val % 10, undefined);
+        prev.next = curr;
+        prev = curr;
+
+        val = Math.floor(val / 10);
+    }
+
+    while (l1 !== null) {
+        val = val + l1.val;
+        l1 = l1.next;
+
+        let curr = new ListNode(val % 10, undefined);
+        prev.next = curr;
+        prev = curr;
+
+        val = Math.floor(val / 10);
+    }
+
+    while (l2 !== null) {
+        val = val + l2.val;
+        l2 = l2.next;
+
+        let curr = new ListNode(val % 10, undefined);
+        prev.next = curr;
+        prev = curr;
+
+        val = Math.floor(val / 10);
+    }
+
+    return dummy.next;
 };
+
+const traverseListNode = function(node) {
+    val = "";
+    while (node != null) {
+        val += node.val + "->";
+        node = node.next;
+    }
+}
+
+let node13 = new ListNode(3, null);
+let node12 = new ListNode(4, node13);
+let node11 = new ListNode(2, node12);
+
+let node23 = new ListNode(4, null);
+let node22 = new ListNode(6, node23);
+let node21 = new ListNode(5, node22);
+
+document.getElementById("add-two-numbers").innerHTML = traverseListNode(addTwoNumbers(node11, node21));
