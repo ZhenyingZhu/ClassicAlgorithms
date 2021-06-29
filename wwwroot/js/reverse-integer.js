9243bb16fcc94b5e08392d7a4f92c232d1c92873
Zhenying Zhu <zz2283@columbia.edu>
2021-06-28 23:18:53 -0700

    Reverse int.

--- wwwroot/js/reverse-integer.js
+++ wwwroot/js/reverse-integer.js
@@ -1,5 +1,30 @@
 const reverse = function(x) {
-    
+    if (x === 0 || x <= -(2 ** 31)) {
+        return 0;
+    }
+
+    if (x === 0) {
+        return 0;
+    }
+
+    let isNegative = x < 0;
+    x = Math.abs(x);
+
+    let res = 0;
+    let cnt = 33;
+    while (x > 0 && cnt > 0) {
+        cnt--;
+        res *= 10;
+        res += x % 10;
+
+        x = Math.floor(x / 10);
+        if (res >= (2 ** 31)) {
+            return 0;
+        }
+        // console.log(x);
+    }
+
+    return isNegative ? -res : res;
 };
 
-document.getElementById("reverse-integer").innerHTML = reverse(10);
+document.getElementById("reverse-integer").innerHTML = reverse(1534236469);
