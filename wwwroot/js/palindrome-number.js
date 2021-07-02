c23c494854ac1872947f52bbb0842df2a422758f
Zhenying Zhu <zz2283@columbia.edu>
2021-07-01 23:09:45 -0700

    IsPalindrome.

---
+++ wwwroot/js/palindrome-number.js
@@ -0,0 +1,36 @@
+const isPalindromeDigit = function(x, n) {
+    console.log("x: " + x + ", n: " + n)
+    if (x < 10 && n <= 1) {
+        return true;
+    }
+
+    let low = x % 10;
+
+    let pow = 10 ** (n - 1);
+    let high = Math.floor(x / pow);
+
+    if (low === high) {
+        x = x % pow;
+        x = Math.floor(x / 10);
+        return isPalindromeDigit(x, n - 2);
+    }
+
+    return false;
+};
+
+const isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+
+    let digit = 0;
+    let pow = 1;
+    while (x >= pow) {
+        digit++;
+        pow *= 10;
+    }
+
+    return isPalindromeDigit(x, digit);
+};
+
+document.getElementById("palindrome-number").innerHTML = isPalindrome(1000021);
