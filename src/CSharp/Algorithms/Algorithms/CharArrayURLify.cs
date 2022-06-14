dc5b15ac79c20c22e2fdb508c3034b5d86048251
Zhenying Zhu <zz2283@columbia.edu>
2022-06-13 23:14:30 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/CharArrayURLify.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Replace space with %20 in place.
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 1.3
+    /// </remarks>
+    public class CharArrayURLify
+    {
+        // Solution 1: Get the total URLify length, then use two pointers backwards.
+        public static void URLifyInplace(char[] urlCharArray, int oriLen)
+        {
+            // Need to deal with oriLen is actually larger than array length.
+            oriLen = Math.Min(urlCharArray.Length, oriLen);
+
+            // Instead of use for loop, just count the space and use math. It is quicker.
+            int totalLen = 0;
+            for (int i = 0; i < oriLen; i++)
+            {
+                if (urlCharArray[i] == ' ')
+                {
+                    totalLen += 3;
+                }
+                else
+                {
+                    totalLen++;
+                }
+            }
+
+            int writer = totalLen - 1;
+            for (int processor = oriLen - 1; processor >= 0; processor--)
+            {
+                if (urlCharArray[processor] == ' ')
+                {
+                    urlCharArray[writer - 2] = '%';
+                    urlCharArray[writer - 1] = '2';
+                    urlCharArray[writer - 0] = '0';
+                    writer -= 3;
+                }
+                else
+                {
+                    urlCharArray[writer--] = urlCharArray[processor];
+                }
+            }
+        }
+    }
+}
---
+++ src/CSharp/Algorithms/AlgorithmsTests/CharArrayURLifyTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms;
+
+namespace AlgorithmsTests
+{
+    [TestClass]
+    public class CharArrayURLifyTest
+    {
+        [TestMethod]
+        public void TestURLifyInplace()
+        {
+            char[] url = CreateCharArray(" a ", 9);
+            CharArrayURLify.URLifyInplace(url, 3);
+            string res = ConstructStringFromCharArray(url);
+            Assert.AreEqual("%20a%20", res);
+        }
+
+        private char[] CreateCharArray(string input, int length)
+        {
+            char[] chars = new char[length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                chars[i] = input[i];
+            }
+
+            return chars;
+        }
+
+        private string ConstructStringFromCharArray(char[] array)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < array.Length && array[i] != '\0'; i++)
+            {
+                sb.Append(array[i]);
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
