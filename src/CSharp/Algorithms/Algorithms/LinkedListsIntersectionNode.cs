49e5dded455e5a5bd83ff527f27f854f6ed60cac
Zhenying Zhu <zz2283@columbia.edu>
2022-06-28 22:23:18 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/LinkedListsIntersectionNode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms.Utils;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Find the intersection node for two linked lists.
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 2.7
+    /// </remarks>
+    public class LinkedListsIntersectionNode
+    {
+        // Solution 1: traverse twice. First count the length. Then skip the first couple nodes of the longer list.
+        public static SinglyLinkedListNode IntersectionCountLength(SinglyLinkedListNode head1, SinglyLinkedListNode head2)
+        {
+            int len1 = 0, len2 = 0;
+            SinglyLinkedListNode ptr1 = head1, ptr2 = head2;
+            while (ptr1.Next != null)
+            {
+                len1++;
+                ptr1 = ptr1.Next;
+            }
+
+            while (ptr2.Next != null)
+            {
+                len2++;
+                ptr2 = ptr2.Next;
+            }
+
+            if (ptr1 != ptr2)
+            {
+                return null;
+            }
+
+            ptr1 = head1;
+            ptr2 = head2;
+            if (len1 > len2)
+            {
+                for (int i = 0; i < len1 - len2; i++)
+                {
+                    ptr1 = ptr1.Next;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < len2 - len1; i++)
+                {
+                    ptr2 = ptr2.Next;
+                }
+            }
+
+            while (ptr1 != ptr2 && ptr1 != null && ptr2 != null)
+            {
+                ptr1 = ptr1.Next;
+                ptr2 = ptr2.Next;
+            }
+
+            return ptr1;
+        }
+    }
+}
---
+++ src/CSharp/Algorithms/AlgorithmsTests/LinkedListsIntersectionNodeTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms;
+using Algorithms.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsTests
+{
+    [TestClass]
+    public class LinkedListsIntersectionNodeTest
+    {
+        [TestMethod]
+        public void TestIntersectionCountLength()
+        {
+            SinglyLinkedListNode head1 = SinglyLinkedListNode.CreateLinkedList(new List<int> { 1, 2, 3, 4, 5 });
+            SinglyLinkedListNode head2 = SinglyLinkedListNode.CreateLinkedList(new List<int> { 6, 7, 8, 9 });
+
+            SinglyLinkedListNode res = LinkedListsIntersectionNode.IntersectionCountLength(head1, head2);
+            Assert.IsNull(res);
+
+            SinglyLinkedListNode intersect = head1.Next.Next;
+            head2.Next.Next.Next.Next = intersect;
+            res = LinkedListsIntersectionNode.IntersectionCountLength(head1, head2);
+            Assert.AreEqual(res, intersect);
+        }
+    }
+}
