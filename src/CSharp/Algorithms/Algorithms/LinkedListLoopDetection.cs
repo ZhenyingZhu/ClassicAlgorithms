81534253830a348f20b412301e57877d1a228ca4
Zhenying Zhu <zz2283@columbia.edu>
2022-06-29 22:20:21 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/LinkedListLoopDetection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms.Utils;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Give a circlar linked list, return the node that is the beginning of the loop.
+    /// Uncertainties:
+    /// 1. Does the loop must exist?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 2.8
+    /// </remarks>
+    public class LinkedListLoopDetection
+    {
+        // Solution 1: two pointers
+        public static SinglyLinkedListNode LoopDetection(SinglyLinkedListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            SinglyLinkedListNode slow = head, fast = head;
+            do
+            {
+                fast = fast.Next.Next;
+                slow = slow.Next;
+            }
+            while (fast != null && fast.Next != null && fast != slow);
+
+            if (fast.Next == null)
+            {
+                return null;
+            }
+
+            fast = head;
+
+            while (fast != slow)
+            {
+                fast = fast.Next;
+                slow = slow.Next;
+            }
+
+            return fast;
+        }
+    }
+}
---
+++ src/CSharp/Algorithms/AlgorithmsTests/LinkedListLoopDetectionTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms;
+using Algorithms.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsTests
+{
+    [TestClass]
+    public class LinkedListLoopDetectionTest
+    {
+        [TestMethod]
+        public void TestLoopDetection()
+        {
+            SinglyLinkedListNode head = SinglyLinkedListNode.CreateLinkedList(new List<int> { 1, 2, 3, 4, 5 });
+            head.Next.Next.Next.Next.Next = head.Next.Next;
+
+            SinglyLinkedListNode loop = LinkedListLoopDetection.LoopDetection(head);
+            Assert.AreEqual(loop, head.Next.Next);
+        }
+    }
+}
