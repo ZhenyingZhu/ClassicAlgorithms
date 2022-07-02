af7fdcd9082a33cc0f54fcbbf0dae20476e8c216
Zhenying Zhu <zz2283@columbia.edu>
2022-07-01 21:49:19 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/ThreeStacksInOneArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Implement 3 stacks in one array.
+    /// Uncertenties:
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 3.1
+    /// </remarks>
+    public class ThreeStacksInOneArray<T>
+    {
+        List<T> list;
+
+        public ThreeStacksInOneArray()
+        {
+            list = new List<T>(30);
+        }
+    }
+}
