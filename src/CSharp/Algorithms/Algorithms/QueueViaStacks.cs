cff813578b38104609e8a98ecf01bf53dd941958
Zhenying Zhu <zz2283@columbia.edu>
2022-07-05 22:19:51 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/QueueViaStacks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Using two stacks to implement a queue.
+    /// Uncertainties:
+    /// 1. 
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 3.4
+    /// </remarks>
+    public class QueueViaStacks
+    {
+        // Solution 1: when peek/dequeue, pop all the elements into another stack first. O(1)
+    }
+}
---
+++ src/CSharp/Algorithms/Algorithms/StackFromSetOfStacks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// When the stack exceed certain threshold, internally start a new stack. Support popAt(stackId).
+    /// Uncertainties:
+    /// 1. When popAt(stackId), should we shift the elements from later stack?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 3.3
+    /// </remarks>
+    public class StackFromSetOfStacks
+    {
+        // Solution 1: Create an array of stacks. Track the top one. When a stack is empty, skip over.
+        // Solution 2: popAt(stackId) moves later stack elements to it. Use linkedList to implement it.
+    }
+}
