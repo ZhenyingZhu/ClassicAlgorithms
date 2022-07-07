322fe3c9252d796611133775b508feba93ea6ba4
Zhenying Zhu <zz2283@columbia.edu>
2022-07-06 23:08:12 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/QueueWithTwoTypeOfElements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Animal Shelter: a queue that can dequeue the oldest animal, or oldest cat/dog.
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 3.6
+    /// </remarks>
+    public class QueueWithTwoTypeOfElements
+    {
+        // Solution 1: 2 queues with a timestamp. When dequeue, dequeue both queues and compare who is older
+    }
+}
---
+++ src/CSharp/Algorithms/Algorithms/SortStackUseStack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Sort a stack use another temp stack. The smallest item is on the top.
+    /// Uncertainties:
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 3.5
+    /// </remarks>
+    public class SortStackUseStack
+    {
+        // Solution 1: use a var to store the pop result of the stack. In the other stack, pop elements out until find a bigger or equal element. Then push the var in.
+    }
+}
