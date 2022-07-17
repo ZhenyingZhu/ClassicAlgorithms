2bf7938b48130692416a2dd2f434a3736ec45f76
Zhenying Zhu <zz2283@columbia.edu>
2022-07-17 11:33:13 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/SequencesToCreateACertainBST.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Give a BST with all distinct value, return all possible sequences that can lead to the tree, when there is no rotate.
+    /// Uncertainties:
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 4.9
+    /// </remarks>
+    public class SequencesToCreateACertainBST
+    {
+        // Solution 1: First element must be to root. Left subtree seq and right subtree seq weave together recursively.
+        // Instead of dup the result, remove the latest added ones and append the new numbers.
+    }
+}
