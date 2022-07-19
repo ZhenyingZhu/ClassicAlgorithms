8ef60daaf7f8eaf5eae882aeca752a3e0623d969
Zhenying Zhu <zz2283@columbia.edu>
2022-07-18 23:36:59 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/CheckSmallTreeIsSubTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Check if a tree is a subtree of another, where the other tree is much bigger.
+    /// Uncertainties:
+    /// How much memory do we have
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 4.10
+    /// </remarks>
+    public class CheckSmallTreeIsSubTree
+    {
+        // Solution 1: traverse tree big until find node that is the root of tree small, then check if left sub tree and right sub tree.
+        // Solution 2: when find the root, do a pre-order traverse of both tree, with leaf node count as null. Then compare the seq.
+    }
+}
