b78c185642ce44f4b5ee9074ce5d30590baca32c
Zhenying Zhu <zz2283@columbia.edu>
2022-07-20 23:30:48 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/BSTSupportsRandomNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Implement a binary search tree, and support return a random node.
+    /// Uncertainties:
+    /// 1. are all nodes uniq?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 4.11
+    /// </remarks>
+    public class BSTSupportsRandomNode
+    {
+        // Solution 1: know how many nodes in total. Each node tracks the order in the tree. Return the nth node.
+    }
+}
