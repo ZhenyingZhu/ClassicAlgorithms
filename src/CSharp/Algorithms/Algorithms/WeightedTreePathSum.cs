5044c0a37da19237ae7f00d41a9d02482e71a9af
Zhenying Zhu <zz2283@columbia.edu>
2022-07-22 21:42:29 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/WeightedTreePathSum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Give a binary tree with weighted nodes. Find paths that sum up to a target. Path can start from middle, but most top down. Return the total count of paths.
+    /// Uncertainties:
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 4.12
+    /// </remarks>
+    public class WeightedTreePathSum
+    {
+        // Solution 1: From a node, record all the sums it can have bottom up.Then use target - current node val to figure out how many from root
+    }
+}
