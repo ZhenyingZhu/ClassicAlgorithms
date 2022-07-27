50b2572e3044d9c7af7d6a0b246fe2cc9d873518
Zhenying Zhu <zz2283@columbia.edu>
2022-07-26 21:37:23 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/BitsToFlipConvertInts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Determin how many bits to flip to convert int A to B.
+    /// Uncertainties:
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 5.6
+    /// </remarks>
+    public class BitsToFlipConvertInts
+    {
+        // Solution 1: Count how many bits are different. O(32)
+        // Solution 2: XOR A and B, then n & (n-1) to clear last 1
+    }
+}
---
+++ src/CSharp/Algorithms/Algorithms/PairwiseBitsSwap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Use at few instructions as possible to swap odd and even bits in an int.
+    /// Uncertainties:
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 5.7
+    /// </remarks>
+    public class PairwiseBitsSwap
+    {
+        // Solution 1: Create a 0101 mask, and a 1010 mask (two ints), and then and the num with them and or together.
+    }
+}
