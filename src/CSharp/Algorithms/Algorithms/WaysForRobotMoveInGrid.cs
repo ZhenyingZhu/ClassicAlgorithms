29a589f656b47636718a1b2f3c69576897aff139
Zhenying Zhu <zz2283@columbia.edu>
2022-08-13 16:06:23 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/WaysForRobotMoveInGrid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// A robot can move down or right. In a grid there are obstacle cells. From top left, find a path to go to bottom right.
+    /// Uncertainties:
+    /// 1. what if no path to return
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 8.2
+    /// </remarks>
+    public class WaysForRobotMoveInGrid
+    {
+        // Solution 1: from top left to down right, find the previous cell that can move from top left to it.
+        // Solution 2: convert to a graph, and use DFS
+    }
+}
