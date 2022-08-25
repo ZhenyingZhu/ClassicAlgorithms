136b1e25e6ac9c341385970dd1acf7ac9592ba95
Zhenying Zhu <zz2283@columbia.edu>
2022-08-24 22:23:24 -0700

    Commit all local changes

--- src/CSharp/Algorithms/Algorithms/NumOfWaysForParenBoolExpression.cs
+++ src/CSharp/Algorithms/Algorithms/NumOfWaysForParenBoolExpression.cs
@@ -7,14 +7,17 @@ namespace Algorithms
     /// <summary>
     /// Give a boolean expression with {0, 1, &, |, ^}, and a target bool value, return the number of ways to parenthesizing
     /// Uncertainties:
-    /// How to not extraneously add parenthesises.
+    /// Can not add parenthesize a solution? It makes the duplicate cases more complicate
     /// </summary>
     /// <remarks>
     /// CC189 6th 8.14
     /// </remarks>
     public class NumOfWaysForParenBoolExpression
     {
-        // Solution 1: break down in each op to find a value
-        // Solution 2: ?
+        // Solution 1: break down in each op to find a value.
+        // Make code consise: totalWays = (leftEvalTrue + leftEvalFalse) * (rightEvalTrue + rightEvalFalse). After cal TotalEvalTrue, TotalEvalFalse = TotalWays - TotalEvalTrue
+        // But it do useless calcuation
+
+        // Solution 2: Use DP. Save the string and its eval result when computed. Better than mem the index because same string can appear in both left and right.
     }
 }
