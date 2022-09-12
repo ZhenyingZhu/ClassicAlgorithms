c1797fced99ed8e55580f5b19fcbf11a32d9f8b8
Zhenying Zhu <zz2283@columbia.edu>
2022-09-11 23:43:02 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/NoSizeSortedArraySearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// A listy data structure which doesn't have size(). Has elementAt(i). If i is bigger than the bound, return -1. It only contains sorted positive integers. Search for the index of element x.
+    /// Uncertainties:
+    /// 1. duplicates? Return any index is good.
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 10.4
+    /// </remarks>
+    public class NoSizeSortedArraySearch
+    {
+        // Solution 1: use binary search. If index > element, search right. But since not know the size, try exponentially going further from 1,2,4,8.
+    }
+}
