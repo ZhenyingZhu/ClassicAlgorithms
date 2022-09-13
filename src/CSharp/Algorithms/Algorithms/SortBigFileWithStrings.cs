1462c2ace1246361780f5e7df77d4227bdf43e34
Zhenying Zhu <zz2283@columbia.edu>
2022-09-12 23:16:43 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/SortBigFileWithStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given a 20GB file with a string each line. How to sort it.
+    /// Uncertenties:
+    /// 1. how large is the memory
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 10.6
+    /// </remarks>
+    public class SortBigFileWithStrings
+    {
+        // Solution 1: external sort. Load 1 MB to memory a time.
+    }
+}
---
+++ src/CSharp/Algorithms/Algorithms/SortedSparseArraySearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// A sorted array of strings, but contains empty strings between. Find the location of a string.
+    /// Uncertainties:
+    /// 1. If search for empty string?
+    /// 2. how many times to search?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 10.5
+    /// </remarks>
+    public class SortedSparseArraySearch
+    {
+        // Solution 1: Linear search until next string, then binary search.
+        // Solution 2: pre-process to compress empty strings so repeat search can be quicker.
+    }
+}
