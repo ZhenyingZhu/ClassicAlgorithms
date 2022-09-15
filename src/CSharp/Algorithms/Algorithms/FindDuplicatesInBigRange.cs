f49857f4a28db104b60497a81c3a3bb5299eaf83
Zhenying Zhu <zz2283@columbia.edu>
2022-09-14 22:49:02 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/FindDuplicatesInBigRange.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given an array with numbers from 1 to N, where N is at most 32k. With 4KB memory, how to print all dups?
+    /// Uncertainties:
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 10.8
+    /// </remarks>
+    public class FindDuplicatesInBigRange
+    {
+        // Solution 1: 4KB is 4*8*1000=32k bits, so a bit vector can be used.
+    }
+}
---
+++ src/CSharp/Algorithms/Algorithms/SortedMatrixSearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given MxN matrix, each row and each column is sorted in ascending order. Find an element.
+    /// Uncertainties:
+    /// 1. return the index?
+    /// 2. can it have dup?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 10.9
+    /// </remarks>
+    public class SortedMatrixSearch
+    {
+        // Solution 1. binary search in two dimensions.
+        // Solution 2: just binary search on the row and linear search on each column.
+    }
+}
