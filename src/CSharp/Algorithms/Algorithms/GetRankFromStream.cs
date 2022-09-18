749dacd968cb85906c2707ca38a2618495891107
Zhenying Zhu <zz2283@columbia.edu>
2022-09-17 23:39:39 -0700

    Commit all local changes

--- src/CSharp/Algorithms/Algorithms/GetRankFromStream.cs
+++ src/CSharp/Algorithms/Algorithms/GetRankFromStream.cs
@@ -5,12 +5,16 @@ using System.Text;
 namespace Algorithms
 {
     /// <summary>
-    /// 
+    /// Given a stream of integers, repeatly getting the rank of a num x (the # of nums <= x not include x itself). track(x) puts the number in the stream. getRankOfNum(x).
+    /// Uncertainties:
+    /// 1. what if the target is less than the min num?
     /// </summary>
     /// <remarks>
     /// CC189 6th 10.10
     /// </remarks>
     public class GetRankFromStream
     {
+        // Solution 1: A linked list record count of each num. When get the rank, linear search to it.
+        // Solution 2: use a binary search tree instead of linked list. The tree node is updated when insert.
     }
 }
---
+++ src/CSharp/Algorithms/Algorithms/PeaksAndValleysInIntArray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// A peak is a num bigger or equals to all its adjacents. A valley is less or equal. Given an int array, sort it into an alternating seq of peaks and valleys.
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 10.11
+    /// </remarks>
+    public class PeaksAndValleysInIntArray
+    {
+        // Solution 1: sort the array, and then swap every two numbers.
+        // Solution 2: no need to sort. Move 2 numbers a time, between 3 numbers, swap the mid one with the largest one.
+    }
+}
