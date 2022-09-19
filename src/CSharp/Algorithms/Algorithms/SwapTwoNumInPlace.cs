9c80be97c7ce7ce0eba455f4902f70edfd2572f1
Zhenying Zhu <zz2283@columbia.edu>
2022-09-18 23:26:37 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/SwapTwoNumInPlace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Swap two numbers without using a var.
+    /// Uncertainties:
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.1
+    /// </remarks>
+    public class SwapTwoNumInPlace
+    {
+        // Solution 1: Use the diff.
+        // Solution 2: Use XOR.
+    }
+}
