00deec527c33bfbbfbbd6505bfbe82e249a96f4c
Zhenying Zhu <zz2283@columbia.edu>
2022-09-19 23:23:35 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/WordFrequencies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Find the freq of occurences of any given word in a book.
+    /// Uncertainties:
+    /// 1. whether to repeatly get the result?
+    /// 2. Is the search case sensitive? need to trim?
+    /// 3. What if the search key has space?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.2
+    /// </remarks>
+    public class WordFrequencies
+    {
+        // Solution 1: Map reduce.
+    }
+}
