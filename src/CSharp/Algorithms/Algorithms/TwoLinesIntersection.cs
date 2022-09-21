d1444777d916b5614444e0571893a0e3b776ba36
Zhenying Zhu <zz2283@columbia.edu>
2022-09-20 23:33:44 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/TwoLinesIntersection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given two line segments with start and end points, compute the intersection if any.
+    /// Uncertainties:
+    /// 1. what if no intersection? 
+    /// 2. if they are two parallel lines?
+    /// 3. if they are same line?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.3
+    /// </remarks>
+    public class TwoLinesIntersection
+    {
+        // Solution 1: use math. All the dots on a line can be represented by (Xa + x, Ya + x(Yb-Ya)/(Xb-Xa)). Xa + x is where the intersection. Then calculate.
+    }
+}
