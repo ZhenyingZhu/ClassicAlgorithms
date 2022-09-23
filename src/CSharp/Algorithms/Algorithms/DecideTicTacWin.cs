386bb8501a443996cd8cc944d484693dd2e328f0
Zhenying Zhu <zz2283@columbia.edu>
2022-09-22 22:26:58 -0700

    Commit all local changes

--- src/CSharp/Algorithms/Algorithms/DecideTicTacWin.cs
+++ src/CSharp/Algorithms/Algorithms/DecideTicTacWin.cs
@@ -18,5 +18,6 @@ namespace Algorithms
         // Solution 2: pre-processing: a hashmap records all the possible results
         // Solution 3: record the board in a way that is easy to get result
         // Solution 4: if know the last move, just check the target row/col/diag
+        // Solution 5: implement an iterator to go over each direction.
     }
 }
