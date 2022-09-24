0cfe12575a23334fce0301204f0b02aa80957f37
Zhenying Zhu <zz2283@columbia.edu>
2022-09-23 18:49:57 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/FactorialTrailingZeroCounts.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Count how many trailing 0s in n factorial.
+    /// Uncertainties:
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.5
+    /// </remarks>
+    public class FactorialTrailingZeroCounts
+    {
+        // Solution 1: 
+    }
+}
