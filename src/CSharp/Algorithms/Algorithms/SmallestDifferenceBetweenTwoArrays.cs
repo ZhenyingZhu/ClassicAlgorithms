efbcdf75c1e0b4b69086a1eba105394d3dd5e866
Zhenying Zhu <zz2283@columbia.edu>
2022-09-25 19:00:21 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/SmallestDifferenceBetweenTwoArrays.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given two arrays of ints, fine the smallest non-negative diff between a pair (one num from each array).
+    /// Uncertainties:
+    /// 1. What if multiple pairs?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.6
+    /// </remarks>
+    public class SmallestDifferenceBetweenTwoArrays
+    {
+        // Solution 1: Sorted, then two pointers.
+    }
+}
