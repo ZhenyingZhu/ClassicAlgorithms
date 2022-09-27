8a7f0fc9203e5bbbb52597055f9e4c654dd5d081
Zhenying Zhu <zz2283@columbia.edu>
2022-09-26 23:28:25 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/FindMaxInTwoWithoutCompare.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Find the max of two numbers, without using if-else or any comparsion operator.
+    /// Uncertainties:
+    /// 1. 
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.7
+    /// </remarks>
+    public class FindMaxInTwoWithoutCompare
+    {
+        // Solution 1: use bit operation. Check if a - b > 0 by shift the result until sign bit. But a - b can overflow, so also need to check if a and b has different sign.
+    }
+}
