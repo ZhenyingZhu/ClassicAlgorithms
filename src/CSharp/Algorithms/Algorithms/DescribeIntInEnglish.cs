dd392276ec26fd65bb61497eb53432bf8f4775b7
Zhenying Zhu <zz2283@columbia.edu>
2022-09-27 22:43:56 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/DescribeIntInEnglish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given an integer, print the enlish phrase.
+    /// Uncertainties:
+    /// 1. Whether to have and?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.8
+    /// </remarks>
+    public class DescribeIntInEnglish
+    {
+        // Solution 1: break the int into every 3 digits. Need to be careful for edge cases.
+    }
+}
