9f14642a93e7d9c1ff04d5067b9119fba5392d86
Zhenying Zhu <zz2283@columbia.edu>
2022-09-28 23:14:24 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/UseAddToImplementOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Implement multiply, substract and divide operations for int, using only add.
+    /// Uncertainties:
+    /// 1. 
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.9
+    /// </remarks>
+    public class UseAddToImplementOperations
+    {
+        // Solution 1: multiply: is add y x times; Substract: add negative num until it is 0; divide: add x n times until large y.
+        // Solution 2: double the value each time to make multiply and substract reduced to O((loga)^2)
+    }
+}
