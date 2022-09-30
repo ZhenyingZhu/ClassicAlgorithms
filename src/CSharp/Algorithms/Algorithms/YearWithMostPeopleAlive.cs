fd2405a6c69f2e33c8239e213209576fbf8cac1b
Zhenying Zhu <zz2283@columbia.edu>
2022-09-29 23:01:20 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/YearWithMostPeopleAlive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given a list of people's birth and dead year, find out the year with most people alive. Assume they all live between 1900 to 2000.
+    /// Uncertainties:
+    /// 1. Open and close of the year?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.10
+    /// </remarks>
+    public class YearWithMostPeopleAlive
+    {
+        // Solution 1: Sort the array by birth year, and use sliding window.
+    }
+}
