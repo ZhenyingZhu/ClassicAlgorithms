c117cd1ee15a6706f078a655473ba6f242c77097
Zhenying Zhu <zz2283@columbia.edu>
2022-10-01 22:23:14 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/PossibleLengthsGivenKBoards.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Building a diving board with k boards. There are 2 types of boards, one is shorter and the other longer. Given the lenths and k, return all the possible length.
+    /// Uncertainties:
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.11
+    /// </remarks>
+    public class PossibleLengthsGivenKBoards
+    {
+        // Solution 1: DP to record k-1 possible lengths.
+        // Solution 2: k ways to pick different types: {0 A, (k-1) B}, {1 A, (k-2) B}, ...
+    }
+}
