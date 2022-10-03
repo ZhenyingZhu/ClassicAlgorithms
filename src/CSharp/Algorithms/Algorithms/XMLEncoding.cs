ea6e43214573cf0583c40ea64ef2d5211c9fdde0
Zhenying Zhu <zz2283@columbia.edu>
2022-10-02 23:46:20 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/XMLEncoding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Encoding an XML element into a string where each tag is mapped to a pre-defined int.
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.12
+    /// </remarks>
+    public class XMLEncoding
+    {
+        // Solution 1:
+    }
+}
