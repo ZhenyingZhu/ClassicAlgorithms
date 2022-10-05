ec02e67104bf312fe905ea93ccb4571bf038665e
Zhenying Zhu <zz2283@columbia.edu>
2022-10-04 23:10:10 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/BisectTwoSquares.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Bisect two squares each in half with one line. Top and bottom sides of the squares are parallel to x-axis.
+    /// Uncertainties:
+    /// 1. How to represent two lines?
+    /// 2. What if two squares exactly overlap?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.13
+    /// </remarks>
+    public class BisectTwoSquares
+    {
+        // Solution 1: Find the mid points of two squares and line them up. If use slope, need to consider the edge case when middles are on the same line
+    }
+}
