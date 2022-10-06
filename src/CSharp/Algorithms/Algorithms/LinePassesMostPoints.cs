7d71405dc28bbca26a3de86762083f74f8180226
Zhenying Zhu <zz2283@columbia.edu>
2022-10-05 23:34:01 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/LinePassesMostPoints.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Find a line passes most points.
+    /// Uncertainties:
+    /// 1. what if points are all collpase?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.14
+    /// </remarks>
+    public class LinePassesMostPoints
+    {
+        // Solution 1: cross each two points, and hash the start point and slope. The slope might not exactly match so need to check if they are within an epsilon.
+    }
+}
