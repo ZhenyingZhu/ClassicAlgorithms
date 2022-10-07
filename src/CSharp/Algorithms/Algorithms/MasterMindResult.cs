e7a79e0de3368eff0e9f0a95eb538c95b0ef049b
Zhenying Zhu <zz2283@columbia.edu>
2022-10-06 23:36:32 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/MasterMindResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Master mind: give a solution with 4 slots that can be any one of RYGB, computer tells whether a hit or a pseudo-hit. Return the result.
+    /// Uncertainties:
+    /// 1. whether pseudo-hit can also be a hit?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.15
+    /// </remarks>
+    public class MasterMindResult
+    {
+        // Solution 1: Scan once and count the slot and hit. Then count again.
+        // Solution 2: scan only once, and minus the hit.
+    }
+}
