7fd19620f60f2b7a4d974f3a0ea33fc6c4317cc5
Zhenying Zhu <zz2283@columbia.edu>
2022-10-08 23:50:32 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/ContiguousSequenceWithLargestSum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given an array, find the contiguous seq with largest sum. Return the sum.
+    /// Uncertainties:
+    /// 1. has negative num?
+    /// 2. if all negative?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.17
+    /// </remarks>
+    public class ContiguousSequenceWithLargestSum
+    {
+        // Solution 1: when the sum become negative, start over. But if all negative, then make sure the sum is also captured.
+    }
+}
