ea4a632ff28880e3ff479fa6901edbbe36c5edf8
Zhenying Zhu <zz2283@columbia.edu>
2022-10-09 23:32:56 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/PatternMatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given a pattern with only a and b, and a string, see if the string can match the pattern.
+    /// 1. What if there are more two kinds of sub-strings?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.18
+    /// </remarks>
+    public class PatternMatch
+    {
+        // Solution 1: Check each substring and try match a and b.
+        // Solution 2: 
+    }
+}
