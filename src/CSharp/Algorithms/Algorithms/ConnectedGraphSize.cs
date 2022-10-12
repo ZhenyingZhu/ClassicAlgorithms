f29f8b1a691f79d47075269f9e55d7637903f23e
Zhenying Zhu <zz2283@columbia.edu>
2022-10-11 23:14:40 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/ConnectedGraphSize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given an int matrix. The value indicates the height of land above sea level.water connected vertically, horizontally or diagonally. Return the size of all ponds.
+    /// Uncertainties:
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.19
+    /// </remarks>
+    public class BelowSeaPondSize
+    {
+        // Solution 1: For each cell, speard out use DFS, and mark cells as a num. Then move to other cells, if is water and not marked, give another number.
+    }
+}
