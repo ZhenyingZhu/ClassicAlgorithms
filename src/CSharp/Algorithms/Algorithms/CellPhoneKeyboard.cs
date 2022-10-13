1ab80ffb91075098f27690c393990c5745941a3f
Zhenying Zhu <zz2283@columbia.edu>
2022-10-12 23:13:22 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/CellPhoneKeyboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// Given a list of valid words, and a seq of number, return the possible valid words.
+    /// Uncertainties:
+    /// 1. 
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.20
+    /// </remarks>
+    public class CellPhoneKeyboard
+    {
+        // Solution 1: Build a trie tree of the words.
+    }
+}
