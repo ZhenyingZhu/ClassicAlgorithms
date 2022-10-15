04c6ac2573095029277905df890b69ac02826e6c
Zhenying Zhu <zz2283@columbia.edu>
2022-10-14 21:56:22 -0700

    Commit all local changes

---
+++ src/CSharp/Algorithms/Algorithms/LangtonAnt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// An ant in a grid of white and black squares. It initially face right. Each step, it flip the color of the square. For white, turn 90 degree closewise and move one unit. Black vice versa.
+    /// Return where it is after k moves.
+    /// Uncertainties:
+    /// 1. is the squares given?
+    /// </summary>
+    /// <remarks>
+    /// CC189 6th 16.22
+    /// </remarks>
+    public class LangtonAnt
+    {
+    }
+}
