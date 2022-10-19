ea25d670e92535c36ef5fcb3a30ae2fee7d787ef
Zhenying Zhu <zz2283@columbia.edu>
2022-10-18 23:02:07 -0700

    Leetcode 11

--- wwwroot/js/container-with-most-water.js
+++ wwwroot/js/container-with-most-water.js
@@ -2,8 +2,7 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    // For each height, find the furtherest height that is longer than it.
+const maxAreaBruteForce = function(height) {
     let max = 0;
     for (let i = 0; i < height.length - 1; i++) {
         for (let j = i + 1; j < height.length; j++) {
@@ -15,4 +14,32 @@ var maxArea = function(height) {
     return max;
 };
 
-document.getElementById("container-with-most-water").innerHTML = maxArea([1,8,6,2,5,4,8,3,7]);
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+ const maxArea = function(height) {
+    // For each height, find the furtherest height that is longer than it.
+    // Use two pointers.
+    if (height=== null || height.length <= 1) {
+        return 0;
+    }
+
+    let max = 0;
+    let i = 0;
+    let j = height.length - 1;
+    while (i < j) {
+        let area = (j - i) * Math.min(height[i], height[j]);
+        max = Math.max(max, area);
+
+        if (height[i] > height[j]) {
+            j--;
+        } else {
+            i++;
+        }
+    }
+
+    return max;
+};
+
+document.getElementById("container-with-most-water").innerHTML = maxArea([1,3,2,5,25,24,5]);
