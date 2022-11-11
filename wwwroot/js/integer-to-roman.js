977f2846d2a05bb0a58a579ea377141b12853b40
Zhenying Zhu <zz2283@columbia.edu>
2022-11-10 23:46:13 -0800

    Int to Roman

--- wwwroot/js/integer-to-roman.js
+++ wwwroot/js/integer-to-roman.js
@@ -3,7 +3,40 @@
  * @return {string}
  */
 const intToRoman = function(num) {
+    // num: [1, 3999]
+    let s = "";
+    s += valToString(Math.floor(num / 1000), 'M', '', '');
+    num = num % 1000;
+    s += valToString(Math.floor(num / 100), 'C', 'D', 'M');
+    num = num % 100;
+    s += valToString(Math.floor(num / 10), 'X', 'L', 'C');
+    num = num % 10;
+    s += valToString(num, 'I', 'V', 'X');
 
+    return s;
 };
 
-document.getElementById("integer-to-roman").innerHTML = intToRoman(12);
+const valToString = function(num, oneSym, fiveSym, tenSym) {
+    let s = "";
+    if (num < 4) {
+        for (let i = 0; i < num; i++) {
+            s += oneSym;
+        }
+    } else if (num === 4) {
+        s += oneSym;
+        s += fiveSym;
+    }
+    else if (num < 9) {
+        s += fiveSym;
+        for (let i = 5; i < num; i++) {
+            s += oneSym;
+        }
+    } else {
+        s += oneSym;
+        s += tenSym;
+    }
+
+    return s;
+}
+
+document.getElementById("integer-to-roman").innerHTML = intToRoman(399);
