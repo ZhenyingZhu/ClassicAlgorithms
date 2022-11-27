21620bcf0d51feaa54eb526735ad956049132cb7
Zhenying Zhu <zz2283@columbia.edu>
2022-11-26 17:56:11 -0800

    Finish letter combination of a phone number

--- wwwroot/js/letter-combinations-of-a-phone-number.js
+++ wwwroot/js/letter-combinations-of-a-phone-number.js
@@ -3,21 +3,43 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    let digitMap = {
-        '2': "abc",
-        '3': "def",
-        '4': "ghi",
-        '5': "jkl",
-        '6': "mno",
-        '7': "pqrs",
-        '8': "tuv",
-        '9': "wxyz"
-    };
-
     let res = [];
-    let com = "";
-    for (let i = 0; i < digits.length; i++) {
-        let poss = digitMap[digits[i]];
+    if (digits === null || digits.length === 0) {
+        return res;
+    }
+
+    appendChar(digits, 0, [], res);
+    return res;
+};
+
+const digitMap = {
+    '2': "abc",
+    '3': "def",
+    '4': "ghi",
+    '5': "jkl",
+    '6': "mno",
+    '7': "pqrs",
+    '8': "tuv",
+    '9': "wxyz"
+};
+
+/**
+ * @param {string} digits
+ * @param {int} idx
+ * @param {string[]} currChars
+ * @param {string[]} res
+ */
+const appendChar = function(digits, idx, currChars, res) {
+    if (idx === digits.length) {
+        res.push(currChars.join(''));
+        return;
+    }
+
+    let poss = digitMap[digits[idx]];
+    for (let i = 0; i < poss.length; i++) {
+        currChars.push(poss[i]);
+        appendChar(digits, idx + 1, currChars, res);
+        currChars.pop();
     }
 };
 
