353f4a86da7b9316aad964e85c07c3877aac5646
Zhenying Zhu <zz2283@columbia.edu>
2022-12-11 23:04:05 -0800

    valid parentheses.

--- wwwroot/js/valid-parentheses.js
+++ wwwroot/js/valid-parentheses.js
@@ -3,12 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const parMap = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
     let stack = [];
     for (let i = 0; i < s.length; i++) {
         if ("([{".includes(s[i])) {
             stack.push(s[i]);
+            continue;
+        }
+
+        if (stack[stack.length - 1] === parMap[s[i]]) {
+            stack.pop();
+        } else {
+            return false;
         }
     }
+
+    return stack.length === 0;
 };
 
-document.getElementById("valid-parentheses").innerHTML = isValid("()");
+document.getElementById("valid-parentheses").innerHTML = isValid("(()[{])}");
