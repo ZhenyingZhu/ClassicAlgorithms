a4d79c472e146f2b2ec332ab34606bd1ccb7468f
Zhenying Zhu <zz2283@columbia.edu>
2022-12-30 17:26:30 -0800

    merge-k-sorted-lists

---
+++ wwwroot/js/merge-k-sorted-lists.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    
+};
+
+document.getElementById("merge-k-sorted-lists").innerHTML = mergeKLists([[1,4,5],[1,3,4],[2,6]]);
