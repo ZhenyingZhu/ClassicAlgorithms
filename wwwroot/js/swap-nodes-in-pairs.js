bfc7ee21e217c32fa2c0404e9d0ab37d1739805e
Zhenying Zhu <zz2283@columbia.edu>
2023-01-06 21:52:22 -0800

    swap-nodes-in-pairs

---
+++ wwwroot/js/swap-nodes-in-pairs.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    
+};
+document.getElementById("swap-nodes-in-pairs").innerHTML = traverseListNode(swapPairs(createList([1, 2, 3, 4])));
