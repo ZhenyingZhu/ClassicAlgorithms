2d0024dc889da36f6b95a1234db62954d36b8c01
Zhenying Zhu <zz2283@columbia.edu>
2023-01-08 20:57:52 -0800

    swap-nodes-in-pairs

--- wwwroot/js/swap-nodes-in-pairs.js
+++ wwwroot/js/swap-nodes-in-pairs.js
@@ -10,6 +10,24 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    
+    let dummy = new ListNode(0);
+    let pre = dummy;
+    let cur = head;
+    while (cur != null && cur.next != null) {
+        let next = cur.next.next;
+        pre.next = swapNodes(cur);
+        pre = cur;
+        cur = next;
+    }
+
+    pre.next = cur;
+    return dummy.next;
 };
-document.getElementById("swap-nodes-in-pairs").innerHTML = traverseListNode(swapPairs(createList([1, 2, 3, 4])));
+
+const swapNodes = function(node) {
+    let next = node.next;
+    next.next = node;
+    return next;
+};
+
+document.getElementById("swap-nodes-in-pairs").innerHTML = traverseListNode(swapPairs(createList([1, 2, 3, 4, 5])));
