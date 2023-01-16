d40b7250d970224ad58c811615a32a96840a61cd
Zhenying Zhu <zz2283@columbia.edu>
2023-01-16 13:19:30 -0800

    stake needle

---
+++ wwwroot/js/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    
+};
+
+document.getElementById("find-the-index-of-the-first-occurrence-in-a-string").innerHTML = strStr('sadbutsad', 'sad');
