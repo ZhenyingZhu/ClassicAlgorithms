370db391e7a36d14281054ecfc11903f5120181f
Zhenying Zhu <zz2283@columbia.edu>
2023-01-20 17:28:46 -0800

    next-permutation

---
+++ wwwroot/js/next-permutation.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    
+};
+
+document.getElementById("next-permutation").innerHTML =
+    nextPermutation([1, 2, 3]);
