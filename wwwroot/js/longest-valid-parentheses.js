3bcb172cbfdf6a3cca21eb5874d1440e5021e2b2
Zhenying Zhu <zz2283@columbia.edu>
2023-01-22 21:41:03 -0800

    longest valid parentheses

--- wwwroot/js/longest-valid-parentheses.js
+++ wwwroot/js/longest-valid-parentheses.js
@@ -3,8 +3,33 @@
  * @return {number}
  */
 var longestValidParentheses = function(s) {
-    // Use DP. d[i][j] means s[i, j] is valid.
-    // d[i][j] = d[i-1][j-1] = T && s[i] s[j] are () || d[i][j-2] = T && s[j-1] s[j] are () || d[i+2][j] = T && s[i] s[i+1] are ()
+    // Use DP. d[j] = i means s[i, j] has unclosed left parentheses.
+    // d[j] = 1. if s[j] = ), from 0 to j - 1, d[]
+
+};
+
+const longestParentheses = function(s, st, ed, d) {
+    if (st > ed) {
+        console.log("doesn't make sense: " + st + " " + ed);
+        return 0;
+    }
+
+    if (st === ed) {
+        d[st][ed] = true;
+        return 0;
+    }
+
+    if (st === ed - 1) {
+        if (s[st] === '(' && s[ed] === ')') {
+            d[st][ed] = true;
+            return 2;
+        } else {
+            d[st][ed] = false;
+            return 0;
+        }
+    }
+
+    if (s[st] === '(' && s[ed] === ')' && d[st][ed])
 };
 
 document.getElementById("longest-valid-parentheses").innerHTML =
