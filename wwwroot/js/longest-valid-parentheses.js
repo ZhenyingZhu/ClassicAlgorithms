f8bf6633f10677ffcc5a2002cea741761341e190
Zhenying Zhu <zz2283@columbia.edu>
2023-01-23 22:31:16 -0800

    finish logest-valid-paretheses

--- wwwroot/js/longest-valid-parentheses.js
+++ wwwroot/js/longest-valid-parentheses.js
@@ -3,34 +3,32 @@
  * @return {number}
  */
 var longestValidParentheses = function(s) {
+    // Solution 1:
     // Use DP. d[j] = i means the length of the longest valid parentheses closed at s[j].
-    // d[j] = 1. if s[j] = ), from 0 to j - 1, d[]
-
-};
-
-const longestParentheses = function(s, st, ed, d) {
-    if (st > ed) {
-        console.log("doesn't make sense: " + st + " " + ed);
-        return 0;
-    }
-
-    if (st === ed) {
-        d[st][ed] = true;
-        return 0;
-    }
-
-    if (st === ed - 1) {
-        if (s[st] === '(' && s[ed] === ')') {
-            d[st][ed] = true;
-            return 2;
+    // init: d[0] = 0 matching s[-1]
+    // if s[i] = '(', d[i+1] = 0;
+    // if s[i] = ')', if s[i-1] = '(', d[i+1] = d[i-1] + 2
+    //                if s[j-1] = ')', using d[j] we can find the first '(' matches s[j-1]. If the previous char is '(', then d[j] = d[j-1-d[j-1]] + 2
+    let res = 0;
+    let d = new Array(s.length + 1);
+    d[0] = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            d[i + 1] = 0;
+        } else if (s[i - 1] === '(') {
+            d[i + 1] = d[i - 1] + 2;
+            res = Math.max(d[i + 1], res);
+        } else if (s[i - 1 - d[i]] === '(') {
+            d[i + 1] = d[i] + 2 + d[i - d[i] - 1];
+            res = Math.max(d[i + 1], res);
         } else {
-            d[st][ed] = false;
-            return 0;
+            d[i + 1] = 0;
         }
     }
 
-    if (s[st] === '(' && s[ed] === ')' && d[st][ed])
+    return res;
+    // Solution 2: use stack
 };
 
 document.getElementById("longest-valid-parentheses").innerHTML =
-    longestValidParentheses("");
+    longestValidParentheses("()(())");
