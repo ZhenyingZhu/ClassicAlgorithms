20fba1c8ac2cf78ff28149a5952eb96d484f8048
Zhenying Zhu <zz2283@columbia.edu>
2023-02-01 23:47:19 -0800

    find-first-and-last-position-of-element-in-sorted-array

---
+++ wwwroot/js/find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    
+};
+
+document.getElementById("find-first-and-last-position-of-element-in-sorted-array").innerHTML =
+searchRange([4,5,6,7,0,1], 6);
