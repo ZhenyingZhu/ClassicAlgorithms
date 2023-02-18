f5a60da8d3c7dbcbdb84903199c2b0f75ab14744
Zhenying Zhu <zz2283@columbia.edu>
2023-02-17 17:27:30 -0800

    Not have my mind clear

--- wwwroot/js/combination-sum.js
+++ wwwroot/js/combination-sum.js
@@ -4,8 +4,27 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-    candidates.sort(function(a, b) { return a - b; });
+    // candidates.sort(function(a, b) { return a - b; });
     
+};
+
+const combinationSumNS = {
+    combinationSumIter: function(candidates, idx, target, cur, res) {
+        // If multiple numbers add up to 0, how to return?
+        if (idx === candidates.length) {
+            return;
+        }
+
+        cur.add(candidates[idx]);
+        if (candidates[idx] === target) {
+            // deep copy?
+            res.add(cur);
+        }
+        this.combinationSumIter(candidates, idx, target - candidates[idx], cur, res);
+
+        cur.pop();
+        this.combinationSumIter(candidates, idx + 1, target - candidates[idx], cur, res);
+    }
 };
 
 document.getElementById("combination-sum").innerHTML = combinationSum([2,3,6,7], 7);
