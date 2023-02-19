147e9a2736cccdb64d1a14d98fe7910b03d61b06
Zhenying Zhu <zz2283@columbia.edu>
2023-02-18 23:22:27 -0800

    combination-sum

--- wwwroot/js/combination-sum.js
+++ wwwroot/js/combination-sum.js
@@ -4,27 +4,36 @@
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-    // candidates.sort(function(a, b) { return a - b; });
-    
+    // If multiple candidates add up to 0, there would be inf res. So it is not possible.
+    // Avoid keep adding the same negative candidate. But negative candidate should still be added. Consider [-1, 2] t=0.
+    // but because the question avoids negative num, so no need to worry about it.
+    candidates.sort(function(a, b) { return a - b; });
+    let res = [];
+    combinationSumNS.combinationSumRecur(candidates, 0, target, [], res);
+    return res;
 };
 
 const combinationSumNS = {
-    combinationSumIter: function(candidates, idx, target, cur, res) {
-        // If multiple numbers add up to 0, how to return?
-        if (idx === candidates.length) {
+    combinationSumRecur: function(candidates, st, target, comb, res) {
+        if (target === 0) {
+            res.push([].concat(comb));
             return;
         }
 
-        cur.add(candidates[idx]);
-        if (candidates[idx] === target) {
-            // deep copy?
-            res.add(cur);
+        if (st === candidates.length) {
+            return;
         }
-        this.combinationSumIter(candidates, idx, target - candidates[idx], cur, res);
 
-        cur.pop();
-        this.combinationSumIter(candidates, idx + 1, target - candidates[idx], cur, res);
+        if (target < candidates[st]) {
+            return;
+        }
+
+        comb.push(candidates[st]);
+        this.combinationSumRecur(candidates, st, target - candidates[st], comb, res);
+        comb.pop();
+        this.combinationSumRecur(candidates, st + 1, target, comb, res);
     }
 };
 
-document.getElementById("combination-sum").innerHTML = combinationSum([2,3,6,7], 7);
+document.getElementById("combination-sum").innerHTML =
+    combinationSum([2,3,6,7], 7);
