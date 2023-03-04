1ef3762c8a66149c0c815899d8f6a6a18dec9a19
Zhenying Zhu <zz2283@columbia.edu>
2023-03-03 21:45:16 -0800

    wildcard-matching

---
+++ wwwroot/js/wildcard-matching.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+    
+};
+
+document.getElementById("wildcard-matching").innerHTML =
+    // isMatch("aa", "a");
+    isMatch("aa", "a");
