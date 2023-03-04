a57b3ce600cd7303787cd37479d144700235e625
Zhenying Zhu <zz2283@columbia.edu>
2023-03-03 21:56:37 -0800

    put an idea

--- wwwroot/js/wildcard-matching.js
+++ wwwroot/js/wildcard-matching.js
@@ -4,7 +4,9 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-    
+    // d[i][j] = T means s(0, i] matches p(0, j].
+    // d[i][j] = (s[i] == p[j] || p[j] == *) && d[i - 1, j - 1] == T
+    //        || p[j] == ? && d[i - x, j - 1] == T
 };
 
 document.getElementById("wildcard-matching").innerHTML =
