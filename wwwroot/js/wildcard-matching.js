40f72aa401c79408228336d083387a8c6ba58a2c
Zhenying Zhu <zz2283@columbia.edu>
2023-03-05 00:06:24 -0800

    wildcard-matching

--- wwwroot/js/wildcard-matching.js
+++ wwwroot/js/wildcard-matching.js
@@ -7,7 +7,10 @@ var isMatch = function(s, p) {
     // d[i][j] = T means s(0, i] matches p(0, j].
     // d[i][j] = (s[i] == p[j] || p[j] == *) && d[i - 1, j - 1] == T
     //        || p[j] == ? && d[i - x, j - 1] == T
-    s[i]
+    let d = new Array(s.length);
+    for (let i = 0; i < s.length; i++) {
+        d[i] = new Array(p.length);
+    }
 };
 
 document.getElementById("wildcard-matching").innerHTML =
