a21e77e77fa58e809fdeed0a9828e6dc1abdeb1d
Zhenying Zhu <zz2283@columbia.edu>
2023-03-04 23:58:39 -0800

    tmp

--- wwwroot/js/wildcard-matching.js
+++ wwwroot/js/wildcard-matching.js
@@ -7,6 +7,7 @@ var isMatch = function(s, p) {
     // d[i][j] = T means s(0, i] matches p(0, j].
     // d[i][j] = (s[i] == p[j] || p[j] == *) && d[i - 1, j - 1] == T
     //        || p[j] == ? && d[i - x, j - 1] == T
+    s[i]
 };
 
 document.getElementById("wildcard-matching").innerHTML =
