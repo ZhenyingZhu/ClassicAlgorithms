f1bd895e56a0eec29ef7b03e2a703b405348fc88
Zhenying Zhu <zz2283@columbia.edu>
2023-03-27 23:15:03 -0700

    permutation-sequence

---
+++ wwwroot/js/permutation-sequence.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var getPermutation = function(n, k) {
+    
+};
+
+document.getElementById("permutation-sequence").innerHTML =
+    getPermutation(3, 3);
