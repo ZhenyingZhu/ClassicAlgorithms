8d9db29a4cdd028f12d0062f581dbbe7205ffa4b
Zhenying Zhu <zz2283@columbia.edu>
2023-04-09 17:06:42 -0700

    text-justification

--- wwwroot/js/text-justification.js
+++ wwwroot/js/text-justification.js
@@ -14,13 +14,20 @@ var fullJustify = function(words, maxWidth) {
     let st = 0;
     while (idx < words.length) {
         let cur = words[idx];
+
+        if (cur.length > maxWidth) {
+            return []; // invalid input case.
+        }
+
         if (len + cur.length > maxWidth) {
             res.push(fullJustifyNS.construstLine(st, idx, words, maxWidth));
             st = idx;
+            len = 0;
             continue;
         }
 
         len += cur.length + 1;
+        idx++;
     }
 
     res.push(fullJustifyNS.construstLastLine(st, idx, words, maxWidth));
@@ -28,16 +35,53 @@ var fullJustify = function(words, maxWidth) {
 };
 
 const fullJustifyNS = {
+    space: " ",
+
     construstLine: function(st, ed, words, maxWidth) {
         // not include ed.
-        return "";
+        let len = 0;
+        for (let i = st; i < ed; i++) {
+            len += words[i].length;
+        }
+
+        let total = maxWidth - len;
+        let inter = ed - st - 1;
+        if (inter === 0) {
+            return words[st] + this.space.repeat(total);
+        }
+
+        let res = "";
+
+        let base = Math.floor(total / inter);
+        let extra = total % inter;
+        let cur = st;
+        for (let i = 0; i < extra; i++) {
+            res += words[cur++] + this.space.repeat(base + 1);
+        }
+
+        while (cur < ed - 1) {
+            res += words[cur++] + this.space.repeat(base);
+        }
+
+        res += words[ed - 1];
+        return res;
     },
 
     construstLastLine: function(st, ed, words, maxWidth) {
         // not include ed.
-        return "";
+        let len = 0;
+        let res = "";
+        for (let i = st; i < ed - 1; i++) {
+            res += words[i] + this.space;
+            len += words[i].length + 1;
+        }
+        res += words[ed - 1];
+        len += words[ed - 1].length;
+        res += this.space.repeat(maxWidth - len);
+        return res;
     },
 };
 
 document.getElementById("text-justification").innerHTML =
-    fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16);
+    // fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16);
+    fullJustify(["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], 20);
