6b645fced3c3d28214b0ef359e0a7e0a49c84f5e
Zhenying Zhu <zz2283@columbia.edu>
2023-05-09 23:02:48 -0700

    combinations not finished

---
+++ wwwroot/js/combinations.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    
+};
+
+document.getElementById("combinations").innerHTML =
+    combine(4, 2);
