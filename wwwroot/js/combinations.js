7dcedb8ea7cd9a0bc2468728bca7f4fa278e2ef2
Zhenying Zhu <zz2283@columbia.edu>
2023-05-10 23:15:04 -0700

    combinations

--- wwwroot/js/combinations.js
+++ wwwroot/js/combinations.js
@@ -4,7 +4,24 @@
  * @return {number[][]}
  */
 var combine = function(n, k) {
-    
+    let res = [];
+    combineNS.combineHelper(1, n, k, [], res);
+    return res;
+};
+
+const combineNS = {
+    combineHelper: function(st, n, k, arr, res) {
+        if (arr.length === k) {
+            res.push(JSON.parse(JSON.stringify(arr)));
+            return;
+        }
+
+        for (let i = st; i <= n; i++) {
+            arr.push(i);
+            this.combineHelper(i + 1, n, k, arr, res);
+            arr.pop();
+        }
+    }
 };
 
 document.getElementById("combinations").innerHTML =
