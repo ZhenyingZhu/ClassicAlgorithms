cb9bad635702e1945ba377709ec9522820501e6d
Zhenying Zhu <zz2283@columbia.edu>
2023-05-23 23:01:33 -0700

    search-in-rotated-sorted-array-ii

--- wwwroot/js/search-in-rotated-sorted-array-ii.js
+++ wwwroot/js/search-in-rotated-sorted-array-ii.js
@@ -8,23 +8,44 @@ var search = function(nums, target) {
     let ed = nums.length - 1;
     while (st + 1 < ed) {
         let md = Math.floor((st + ed) / 2);
-        if (nums[md] === target) {
+        if (nums[md] === target || nums[st] === target || nums[ed] === target) {
             return true;
         }
 
-        if (nums[st] < nums[ed]) {
+        if (nums[st] === nums[ed]) {
+            st++;
+            ed--;
+        }
+        else if (nums[st] < nums[ed]) {
+            // definitely no rotate in between.
             if (nums[md] > target) {
                 ed = md;
             } else {
                 st = md;
             }
-        } else if (nums[st] > nums[ed]) {
-            if (nums[md] > nums[st]) {
-                if (nums[md])
+        } else if (nums[st] < nums[md]) {
+            // definitely no rotate in first half.
+            if (nums[md] < target) {
+                st = md;
+            } else if (nums[st] < target) {
+                ed = md;
+            } else {
+                st = md;
+            }
+        } else if (nums[md] < nums[ed]) {
+            if (target < nums[md]) {
+                ed = md;
+            } else if (nums[ed] < target) {
+                ed = md;
+            } else {
+                st = md;
             }
         }
     }
+
+    return nums[st] === target || nums[ed] === target;
 };
 
 document.getElementById("search-in-rotated-sorted-array-ii").innerHTML =
-    search([2,5,6,0,0,1,2], 0);
+    // search([2,5,6,0,0,1,2], 0);
+    search([2,5,6,0,0,1,2], 3);
