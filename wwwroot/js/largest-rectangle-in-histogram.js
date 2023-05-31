a7128f946cf52131c96cbd51571030f67a1fc4db
Zhenying Zhu <zz2283@columbia.edu>
2023-05-30 22:59:31 -0700

    largest-rectangle-in-histogram not finished

--- wwwroot/js/largest-rectangle-in-histogram.js
+++ wwwroot/js/largest-rectangle-in-histogram.js
@@ -3,26 +3,34 @@
  * @return {number}
  */
 var largestRectangleArea = function(heights) {
-    // area = min(x ~ y) * (y - x + 1)
-    // Use DP to find min(x ~ y)
-    if (heights.length === 0) {
+    // area = minHeight(x, y) * (y - x + 1)
+    // minHeight(x, y) = min(minHeight(x, y - 1), height(y))
+    let m = heights.length;
+    if (m === 0) {
         return 0;
     }
 
-    for (let i = 0; i < heights.length; i++) {
-
+    let matrix = new Array(m);
+    for (let i = 0; i < m; i++) {
+        matrix[i] = new Array(m);
     }
-};
 
-let largestRectangleAreaNS = {
-    helper: function(heights, end) {
-        let max = 0;
-        let 
-        for (let i = 0; i < end; i++) {
+    let maxArea = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = i; j < m; j++) {
+            if (j === i) {
+                matrix[i][j] = heights[i];
+            } else {
+                matrix[i][j] = Math.min(matrix[i][j - 1], heights[j]);
+            }
 
+            maxArea = Math.max(maxArea, matrix[i][j] * (j - i + 1));
         }
     }
+
+    return maxArea;
 };
 
 document.getElementById("largest-rectangle-in-histogram").innerHTML =
-    traverseListNode(deleteDuplicates(createList([0,1,1,1,2,2])));
+    // largestRectangleArea([2,1,5,6,2,3]);
+    largestRectangleArea([2,4]);
