a2b3b27870393a9379cf923e3cd9fe98e56f75b7
Zhenying Zhu <zz2283@columbia.edu>
2023-06-06 22:59:50 -0700

    maximal-rectangle not finished

---
+++ wwwroot/js/maximal-rectangle.js
@@ -0,0 +1,14 @@
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalRectangle = function(matrix) {
+    // seems like can combine largestrectranglearea with this.
+};
+
+document.getElementById("maximal-rectangle").innerHTML =
+    // largestRectangleArea([2,1,5,5,6,2,3]);
+    // largestRectangleArea([2,4]);
+    // largestRectangleArea([2,1,2]);
+    // largestRectangleArea([0,9]);
+    largestRectangleArea([0,1,0,1]);
