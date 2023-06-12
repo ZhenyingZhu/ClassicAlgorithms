d08e4971bb03bd618cfbdae165c00b6c3bda006f
Zhenying Zhu <zz2283@columbia.edu>
2023-06-11 21:20:55 -0700

    maximal-rectangle not finished

--- wwwroot/js/maximal-rectangle.js
+++ wwwroot/js/maximal-rectangle.js
@@ -4,6 +4,35 @@
  */
 var maximalRectangle = function(matrix) {
     // Use 2 DPs.
+    let m = matrix.length;
+    let n = matrix[0].length;
+
+    let heights = new Array(m);
+    for (let i = 0; i < m; i++) {
+        heights[i] = new Array(n);
+    }
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === "0") {
+                heights[i][j] = 0;
+            } else if (i === 0) {
+                heights[i][j] = 1;
+            } else {
+                heights[i][j] = heights[i - 1][j] + 1;
+            }
+        }
+    }
+
+    let maxArea = 0;
+    for (let i = 0; i < m; i++) {
+        let jStack = [-1];
+        let hStack = [-1];
+        let j = 0;
+        while (j < n) {
+
+        }
+    }
 };
 
 document.getElementById("maximal-rectangle").innerHTML =
