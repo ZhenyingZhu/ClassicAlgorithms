14538b04777eca4f173d245f4a27b07eb4d3df91
Zhenying Zhu <zz2283@columbia.edu>
2023-06-13 22:18:00 -0700

    partition-list

--- wwwroot/js/partition-list.js
+++ wwwroot/js/partition-list.js
@@ -11,8 +11,30 @@
  * @return {ListNode}
  */
 var partition = function(head, x) {
-    
+    let smaller = new ListNode();
+    let equalGreater = new ListNode();
+    let start = smaller;
+    let mid = equalGreater;
+
+    let cur = head;
+    while (cur != null) {
+        if (cur.val < x) {
+            smaller.next = cur;
+            smaller = smaller.next;
+            console.log("smaller: " + smaller.val);
+        } else {
+            equalGreater.next = cur;
+            equalGreater = equalGreater.next;
+            console.log("greater: " + equalGreater.val);
+        }
+        cur = cur.next;
+    }
+
+    smaller.next = mid.next;
+    equalGreater.next = null;
+
+    return start.next;
 };
 
 document.getElementById("partition-list").innerHTML =
-    threeSumClosest([-1, 2, 1, -4], 1);
+    traverseListNode(partition(createList([1,4,3,2,5,2]), 3));
