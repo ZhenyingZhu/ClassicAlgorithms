9094c975f6fd26023c6d2cb28574d586fc794f85
Zhenying Zhu <zz2283@columbia.edu>
2023-06-14 22:30:46 -0700

    scramble-string not finished

---
+++ wwwroot/js/scramble-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var isScramble = function(s1, s2) {
+    
+};
+
+document.getElementById("scramble-string").innerHTML =
+    traverseListNode(partition(createList([1,4,3,2,5,2]), 3));
