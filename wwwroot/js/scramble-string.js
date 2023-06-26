7ca3cd4c23dff9eb827c6ad7026efa7c7f5afd5e
Zhenying Zhu <zz2283@columbia.edu>
2023-06-25 22:13:43 -0700

    scramble-string

--- wwwroot/js/scramble-string.js
+++ wwwroot/js/scramble-string.js
@@ -4,8 +4,8 @@
  * @return {boolean}
  */
 var isScramble = function(s1, s2) {
-    
+    // Use DP. dp[i][j][k] means whether s1[i:i+k] is a scramble of s2[j:j+k].
 };
 
 document.getElementById("scramble-string").innerHTML =
-    isScramble();
+    isScramble("great", "rgeat");
