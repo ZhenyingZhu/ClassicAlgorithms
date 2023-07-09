7c4466cd55568f29dc997c36d0f0db76e2bad7b2
Zhenying Zhu <zz2283@columbia.edu>
2023-07-08 22:20:24 -0700

    scramble string not finished

--- wwwroot/js/scramble-string.js
+++ wwwroot/js/scramble-string.js
@@ -4,8 +4,8 @@
  * @return {boolean}
  */
 var isScramble = function(s1, s2) {
-    // Use DP. dp[i][j][k] means whether s1[i:i+k] is a scramble of s2[j:j+k].
-    // dp[i][j][k] = for x in 0:k, (dp[i][j+k-x][x] && dp[i+k-x][j][k-x]) || (dp[i][j][x] && dp[i+k-x][j+k-x][k-x])
+    // Use DP. dp[i][j][k] means whether s1[i:i+k) is a scramble of s2[j:j+k])
+    // dp[i][j][k] = for l in [1:k), if any (dp[i][j+k-l][l] && dp[i+k-l][j][k-l]) || (dp[i][j][x] && dp[i+k-x][j+k-x][k-x])
 };
 
 const isScrambleNS = {
