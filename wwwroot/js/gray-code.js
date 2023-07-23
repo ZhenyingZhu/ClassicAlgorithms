b324732c79183093fe5c7d44e54501ddbe8b9624
Zhenying Zhu <zz2283@columbia.edu>
2023-07-22 22:37:15 -0700

    gray-code finished

--- wwwroot/js/gray-code.js
+++ wwwroot/js/gray-code.js
@@ -3,8 +3,22 @@
  * @return {number[]}
  */
 var grayCode = function(n) {
-    
+    if (n === 0) {
+        return [];
+    }
+
+    let res = [0, 1];
+    let mul = 1;
+    for (let i = 1; i < n; i++) {
+        mul *= 2;
+        let l = res.length;
+        for (let j = l - 1; j >= 0; j--) {
+            res.push(mul + res[j]);
+        }
+    }
+
+    return res;
 };
 
 document.getElementById("gray-code").innerHTML =
-    grayCode(1);
+    grayCode(3);
