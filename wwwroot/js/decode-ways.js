ab0c39fff30a85738a3d86a75aa557d6bd1ecabd
Zhenying Zhu <zz2283@columbia.edu>
2023-08-03 23:35:21 -0700

    decode-ways

---
+++ wwwroot/js/decode-ways.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    // dp[x] is the total ways. dp[x] = 
+};
+
+document.getElementById("decode-ways").innerHTML =
+    numDecodings("12");
