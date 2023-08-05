5f5246af137276a107b38745865ac508bbce7ac7
Zhenying Zhu <zz2283@columbia.edu>
2023-08-05 16:43:49 -0700

    decode-ways finished

--- wwwroot/js/decode-ways.js
+++ wwwroot/js/decode-ways.js
@@ -17,14 +17,19 @@ var numDecodings = function(s) {
     dp[0] = 1;
     dp[1] = 1;
     for (let i = 1; i < n; i++) {
-        if (s[i] === '0') {
-            dp[i + 1] = dp[i] - 1;
-        } else if (s[i - 1] === '0' ||
-            s[i - 1] > '2' ||
-            s[i - 1] === '2' && ) {
-            dp[i + 1] = dp[i - 1];
-        } else {
-            dp[i + 1] = dp[i - 1] + dp[i];
+        dp[i + 1] = 0;
+
+        if (s[i] !== '0') {
+            dp[i + 1] = dp[i];
+        }
+
+        if (s[i - 1] === '1' ||
+        (s[i - 1] === '2' && s[i] <= '6')) {
+            dp[i + 1] += dp[i - 1];
+        }
+
+        if (dp[i + 1] === 0) {
+            return 0;
         }
     }
 
@@ -32,4 +37,6 @@ var numDecodings = function(s) {
 };
 
 document.getElementById("decode-ways").innerHTML =
-    numDecodings("102");
+    // numDecodings("1102");
+    // numDecodings("901");
+    numDecodings("2611055971756562");
