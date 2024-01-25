38a9ae3d9d99bcbfd84901a27f09b5520ebb31e2
Zhenying Zhu <zz2283@columbia.edu>
2024-01-24 22:56:52 -0800

    Still not able to make it right.

--- src/CSharp/Algorithms/Algorithms/AsFarFromLandAsPossible.cs
+++ src/CSharp/Algorithms/Algorithms/AsFarFromLandAsPossible.cs
@@ -11,9 +11,11 @@ namespace Algorithms
     /// </remarks>
     public class AsFarFromLandAsPossible
     {
-        public int MaxDistance(int[][] grid)
+        public static int MaxDistance(int[][] grid)
         {
             int n = grid.Length;
+
+            if (n == 0) return 0;
 
             int[][] dist = new int[n][];
             for (int i = 0; i < n; i++)
@@ -21,7 +23,7 @@ namespace Algorithms
                 dist[i] = new int[n];
                 for (int j = 0; j < n; j++)
                 {
-                    dist[i][j] = grid[i][j] == 1 ? 0 : int.MaxValue;
+                    dist[i][j] = int.MaxValue;
                 }
             }
 
@@ -48,48 +50,56 @@ namespace Algorithms
                 }
             }
 
-            return maxDist == 0 ? - 1 : maxDist;
+            return (maxDist == int.MaxValue || maxDist == 0) ? - 1 : maxDist;
         }
 
-        private void BFS(int[][] grid, int[][] dist, int i, int j)
+        private static void BFS(int[][] grid, int[][] dist, int i, int j)
         {
             int n = grid.Length;
 
             Queue<int> xQueue = new();
             Queue<int> yQueue = new();
+            Queue<int> dQueue = new();
             xQueue.Enqueue(i);
             yQueue.Enqueue(j);
 
             int curDist = 0;
+            dQueue.Enqueue(curDist);
+
             while (xQueue.Count > 0)
             {
                 int x = xQueue.Dequeue();
                 int y = yQueue.Dequeue();
+                int d = dQueue.Dequeue();
 
                 if (x < 0 || x >= n || y < 0 || y >= n)
                 {
                     continue;
                 }
 
-                if (dist[x][y] <= curDist)
+                if (dist[x][y] <= d + 1)
                 {
                     continue;
                 }
 
                 // ?
-                dist[x][y] = curDist;
+                dist[x][y] = d;
 
                 xQueue.Enqueue(x - 1);
                 yQueue.Enqueue(y);
+                dQueue.Enqueue(d + 1);
 
                 xQueue.Enqueue(x + 1);
                 yQueue.Enqueue(y);
+                dQueue.Enqueue(d + 1);
 
                 xQueue.Enqueue(x);
                 yQueue.Enqueue(y - 1);
+                dQueue.Enqueue(d + 1);
 
                 xQueue.Enqueue(x);
                 yQueue.Enqueue(y + 1);
+                dQueue.Enqueue(d + 1);
             }
         }
     }
---
+++ src/CSharp/Algorithms/AlgorithmsTests/AsFarFromLandAsPossibleTest.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsTests
+{
+    [TestClass]
+    public class AsFarFromLandAsPossibleTest
+    {
+        [TestMethod]
+        public void TestMaxDistance()
+        {
+            //Assert.AreEqual(2, AsFarFromLandAsPossible.MaxDistance([[1, 0, 1], [0, 0, 0], [1, 0, 1]]));
+            //Assert.AreEqual(4, AsFarFromLandAsPossible.MaxDistance([[1, 0, 0], [0, 0, 0], [0, 0, 0]]));
+            Assert.AreEqual(-1, AsFarFromLandAsPossible.MaxDistance([[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]));
+        }
+    }
+}
